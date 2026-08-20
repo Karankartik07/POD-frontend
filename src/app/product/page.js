39db3f97bcae9287cdb8208bf4f1a4ca4913e784
Karankartik07 <unknownkartik07@gmail.com>
2026-08-20 19:01:10 +0530

    added new changes

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,13 +1,20 @@
+import { Suspense } from "react";
 import Product from "../../Components/Product/ProductMain/Product";
 import AdditionalInfo from "../../Components/Product/AdditonInfo/AdditionalInfo";
 import RelatedProducts from "../../Components/Product/RelatedProducts/RelatedProducts";
 
 export default function Page() {
   return (
-    <>
+    <Suspense
+      fallback={
+        <div style={{ textAlign: "center", padding: "100px 0", fontSize: "18px", color: "#666", fontWeight: "500" }}>
+          Loading product page...
+        </div>
+      }
+    >
       <Product />
       <AdditionalInfo />
       <RelatedProducts />
-    </>
+    </Suspense>
   );
 }
