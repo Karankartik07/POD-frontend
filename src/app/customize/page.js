196b1e56c87b7acc04e761b5aa438c1ba3b272ae
Karankartik07 <unknownkartik07@gmail.com>
2026-08-25 17:00:46 +0530

    new changes bugs and theme

--- src/app/customize/page.js
+++ src/app/customize/page.js
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
 import CustomizePage from "../../Components/Customize/CustomizePage";
 
 export default function Page() {
-  return <CustomizePage />;
+  return (
+    <Suspense fallback={<div style={{ textAlign: "center", padding: "60px", color: "#666" }}>Loading visualizer...</div>}>
+      <CustomizePage />
+    </Suspense>
+  );
 }
+
